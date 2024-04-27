a6a0cf164222c4aa67a95f3bf22a03fb69622ed1
Pushpak Vattam <vattampushpak243@gmail.com>
2024-04-27 08:32:47 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

--- roles/common/templates/nodejs.sh
+++ roles/common/templates/nodejs.sh
@@ -1,5 +1,5 @@
 cd /app
-NEW_RELIC_APP_NAME= {{component}} NEW_RELIC_LICENSE_KEY={{ lookup('amazon.aws.aws_ssm', 'newrelic.licence_key', region='us-east-1' ) }} node -r newrelic server.js
+NEW_RELIC_APP_NAME= {{component}} NEW_RELIC_LICENSE_KEY={{ lookup('amazon.aws.aws_ssm', 'newrelic.key', region='us-east-1' ) }} node -r newrelic server.js
 
 #{{component}} NEW_RELIC_LICENSE_KEY= {{ lookup ('amazon.aws.aws_ssm', 'newrelic.key', region='us-east-1' ) }} node -r newrelic server.js
 
