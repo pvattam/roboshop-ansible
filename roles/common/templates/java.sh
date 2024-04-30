f611a78116525a07ea89dced731a80a7fc165011
Pushpak Vattam <vattampushpak243@gmail.com>
2024-04-30 09:06:59 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

---
+++ roles/common/templates/java.sh
@@ -0,0 +1,2 @@
+cd /app
+java -javaagent:opt/newrelic/newrelic.jar -jar app.jar /app/{{component}}.jar
---
+++ roles/common/templates/python.sh
@@ -0,0 +1,4 @@
+cd /app
+
+
+NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin /usr/local/bin/uwsgi --ini payment.ini
